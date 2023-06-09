07461bb787417c0640dfc574f226eef768a64718
Julio Cesar Melegari <juliocesar.melegari@gmail.com>
2023-06-09 00:30:08 -0300

    add logic, finished project

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import quizz_questions from '../../../assets/data/quizz_questions.json';
 
 @Component({
   selector: 'app-quizz',
@@ -7,9 +8,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizzComponent implements OnInit {
 
+  title:string=""
+  questions:any
+  questionSelected:any
+
+  answers:string[]=[]
+  answersSelected:string=""
+
+  questionIndex:number=0 /**ponteiro atual das questoes */
+  questionMaxIndex:number=0
+
+  finished:boolean=false
+
   constructor() { }
 
   ngOnInit(): void {
+    if(quizz_questions){
+      this.finished=false
+      this.title=quizz_questions.title
+
+      this.questions=quizz_questions.questions
+      this.questionSelected=this.questions[this.questionIndex]
+
+      this.questionIndex=0
+      this.questionMaxIndex=this.questions.length
+    }
+  }
+
+  playerChoose(value:string){
+    this.answers.push(value)
+    /*console.log(this.answers)
+    console.log("teste")*/
+    this.nextStep()
+  }
+
+  async nextStep(){
+    this.questionIndex+=1
+
+    if(this.questionMaxIndex > this.questionIndex){
+      this.questionSelected = this.questions[this.questionIndex]
+    }else{
+      const finalAnswer:string = await this.checkResult(this.answers)
+      this.finished=true
+      //verifica opcao ganhadora
+      this.answersSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results]
+    }
+  }
+
+  async checkResult(anwsers:string[]){
+    //algoritmo que percorre o array verificando qual elemento aparece mais
+    const result = anwsers.reduce((previous, current, i, arr)=>{
+      if(
+        arr.filter(item => item === previous).length >
+        arr.filter(item => item === current).length
+      ){
+        return previous
+      }else{
+        return current
+      }
+    })
+    return result
   }
 
 }
